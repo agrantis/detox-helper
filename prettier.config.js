c3457b98846f7fd66dda848db9c8e06ecf47dcb8
Mikael Goglichidze <goglichidze.mike@gmail.com>
2024-01-22 15:14:21 +0400

    fix: add and run prettier

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line import/no-commonjs
+module.exports = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  useTabs: true,
+  printWidth: 80,
+  bracketSpacing: true,
+  importOrderSortSpecifiers: true,
+  importOrderSeparation: false,
+  importOrderGroupNamespaceSpecifiers: true,
+  proseWrap: "always",
+};
